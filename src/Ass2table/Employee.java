e02ec9b4443f09939ce0415d9bc65476b59344a5
Shivani Rahatwad <rahatwad.s@northeastern.edu>
2022-10-01 14:57:09 -0400

    Employee.java class

---
+++ src/Ass2table/Employee.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ass2table;
+
+/**
+ *
+ * @author shivanirahatwad
+ */
+public class Employee {
+    
+    private String name;
+    private String EmployeeID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeID() {
+        return EmployeeID;
+    }
+
+    public void setEmployeeID(String EmployeeID) {
+        this.EmployeeID = EmployeeID;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getStartdate() {
+        return Startdate;
+    }
+
+    public void setStartdate(String Startdate) {
+        this.Startdate = Startdate;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+    public String getTeaminfo() {
+        return Teaminfo;
+    }
+
+    public void setTeaminfo(String Teaminfo) {
+        this.Teaminfo = Teaminfo;
+    }
+
+    public String getPositiontitle() {
+        return Positiontitle;
+    }
+
+    public void setPositiontitle(String Positiontitle) {
+        this.Positiontitle = Positiontitle;
+    }
+
+
+
+    public String getPhno() {
+        return Phno;
+    }
+
+    public void setPhno(String Phno) {
+        this.Phno = Phno;
+    }
+
+    public String getEmailadd() {
+        return emailadd;
+    }
+
+    public void setEmailadd(String emailadd) {
+        this.emailadd = emailadd;
+    }
+    private int Age;
+    private String Gender;
+    private String  Startdate;
+    private String Level;
+   private String  Teaminfo;
+    private String Positiontitle;
+    private String Phno;
+    private String emailadd;
+    
+             
+  
+}
